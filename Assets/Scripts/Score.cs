e16df7db9e6ba89b41dc0e5fc8840d0bc1d3a7b0
Francis Acheampong <francisacheampong@gmail.com>
2018-04-14 20:54:49 -0400

    first commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private int currentScore;
+    private Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentScore = 0;
+        scoreText = GetComponent<Text>();
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        //currentScore ++;
+        //UpdateText();
+    }
+
+    //void UpdateText()
+    //{
+    //    scoreText.text = "Score: " + currentScore;
+    //}
+}
